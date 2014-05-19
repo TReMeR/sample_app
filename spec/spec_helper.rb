f1eaeef5b5675a8261fce34e2003809143d85688
TReMeR <pumpedgeek@gmail.com>
2014-05-19 12:55:55 +0300

    Fihish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,6 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  #adding Capybara DSL to Helpers config
+  config.include Capybara::DSL
 end
